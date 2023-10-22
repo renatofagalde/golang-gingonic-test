9e1cfddf3089f1bc78b07ef5ba990bc3ea101426
Renato Fagalde <renato@likwi.com.br>
2023-10-22 20:58:24 -0300

    Initial commit

---
+++ get_user_id.go
@@ -0,0 +1,16 @@
+package golang_gingonic_test
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+func GetUserId(c *gin.Context) {
+	fmt.Println(c.Query("foo"))
+	fmt.Println(c.Param("id"))
+
+	id, _ := strconv.Atoi(c.Param("id"))
+	c.JSON(http.StatusOK, id)
+}
